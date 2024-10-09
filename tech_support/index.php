a910166f39c4ec84c0343b2d8e66959b5aa00c3d
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-10-09 11:48:28 -0400

    Initial files

---
+++ tech_support/index.php
@@ -0,0 +1,35 @@
+<?php   
+    require("db.php");
+    $querycCustomers = 'SELECT * FROM customers';
+    $statement1 = $db->prepare($querycCustomers);
+    $statement1->execute();
+    $products = $statement1->fetchAll();
+    $statement1->closeCursor();
+?>
+<?php include 'view/header.php'; ?>
+<main>
+    <nav>
+        
+    <h2>Administrators</h2>
+    <ul>
+        <li><a href="product_manager">Manage Products</a></li>
+        <li><a href="under_construction.php">Manage Technicians</a></li>
+        <li><a href="customer.php">Manage Customers</a></li>
+        <li><a href="under_construction.php">Create Incident</a></li>
+        <li><a href="under_construction.php">Assign Incident</a></li>
+        <li><a href="under_construction.php">Display Incidents</a></li>
+    </ul>
+
+    <h2>Technicians</h2>    
+    <ul>
+        <li><a href="under_construction.php">Update Incident</a></li>
+    </ul>
+
+    <h2>Customers</h2>
+    <ul>
+        <li><a href="under_construction.php">Register Product</a></li>
+    </ul>
+    
+    </nav>
+</section>
+<?php include 'view/footer.php'; ?>
